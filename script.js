e5008dfe149a36619ec8a3d533c965e17ed7db3c
Katarina Roberts <katarinarobertsux@gmail.com>
2022-05-13 15:34:34 +0100

    Update script.js
    update

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const player1Btn = document.querySelector('#player1-btn');
-const player2Btn = document.querySelector('#player2-btn');
+const player1 = {
+   score: 0,
+   button: document.querySelector('#player1-btn'),
+   display: document.querySelector('#player1-score-el')
+};
+
+const player2 = {
+   score: 0,
+   button: document.querySelector('#player2-btn'),
+   display: document.querySelector('#player2-score-el')
+};
+
 const resetBtn = document.querySelector('#reset-btn');
-const player1ScoreEl = document.querySelector('#player1-score-el');
-const player2ScoreEl = document.querySelector('#player2-score-el');
 const scoreSelect = document.querySelector('#score-select');
 
 let maxScore;
-let player1Score = 0;
-let player2Score = 0;
 let isGameOver = true;
 
 scoreSelect.addEventListener('change', function () {
@@ -16,34 +22,38 @@ scoreSelect.addEventListener('change', function () {
    isGameOver = false;
 })
 
-player1Btn.addEventListener('click', function(){
-   console.log(isGameOver);
+function updateScore (player, opponent) {
    if (!isGameOver){
-      player1Score +=1;
-      if (player1Score === maxScore) {
+      player.score +=1;
+      if (player.score === maxScore) {
          isGameOver = true;
-         player1ScoreEl.classList.add('has-text-success');
-         player2ScoreEl.classList.add('has-text-danger');
-         player1Btn.disabled = true;
-         player2Btn.disabled = true;
+         player.display.classList.add('has-text-success');
+         opponent.display.classList.add('has-text-danger');
+         player.button.disabled = true;
+         opponent.button.disabled = true;
       }
-      player1ScoreEl.innerHTML = `${player1Score}`;
+      player.display.innerHTML = `${player.score}`;
    }
+}
+
+player1.button.addEventListener('click', function(){
+   updateScore(player1, player2);
+   // console.log(isGameOver);
+   // if (!isGameOver){
+   //    player1Score +=1;
+   //    if (player1Score === maxScore) {
+   //       isGameOver = true;
+   //       player1ScoreEl.classList.add('has-text-success');
+   //       player2ScoreEl.classList.add('has-text-danger');
+   //       player1Btn.disabled = true;
+   //       player2Btn.disabled = true;
+   //    }
+   //    player1ScoreEl.innerHTML = `${player1Score}`;
+   // }
 })
 
-player2Btn.addEventListener('click', function(){
-   console.log(isGameOver);
-   if (!isGameOver){
-      player2Score +=1;
-      if (player2Score === maxScore) {
-         isGameOver = true;
-         player2ScoreEl.classList.add('has-text-success');
-         player1ScoreEl.classList.add('has-text-danger');
-         player1Btn.disabled = true;
-         player2Btn.disabled = true;
-      }
-      player2ScoreEl.innerHTML = `${player2Score}`;
-   }
+player2.button.addEventListener('click', function(){
+   updateScore(player2, player1);
 })
 
 resetBtn.addEventListener('click', function () {
@@ -52,17 +62,17 @@ resetBtn.addEventListener('click', function () {
 })
 
 function reset (){
-   player1Score = 0;
-   player2Score = 0;
+   player1.score = 0;
+   player2.score = 0;
    maxScore = 0;
-   player1ScoreEl.innerHTML = `${player1Score}`;
-   player2ScoreEl.innerHTML = `${player2Score}`;
+   player1.display.innerHTML = `${player1.score}`;
+   player2.display.innerHTML = `${player2.score}`;
 }
 
 function newGame() {
    scoreSelect.selectedIndex = 0;
-   player1ScoreEl.classList.remove('has-text-success', 'has-text-danger');
-   player2ScoreEl.classList.remove('has-text-success','has-text-danger');
-   player1Btn.disabled = false;
-   player2Btn.disabled = false;
+   player1.display.classList.remove('has-text-success', 'has-text-danger');
+   player2.display.classList.remove('has-text-success','has-text-danger');
+   player1.button.disabled = false;
+   player2.button.disabled = false;
 }
